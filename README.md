4344327f8655bf9415ab50a19a1850ce2ca3dd4c
VHSgunzo <wosganzo@gmail.com>
2023-01-26 04:56:34 +0300

    v0.0.4

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # superglue
-Superglue for gluing runtime and squashfs image or shell script and archive
+Tool for write a small file to the beginning of a large file in place.
+This can be useful for gluing runtime and squashfs image or shell script and archive.
 
 ## To get started:
 * **Download the latest revision**
@@ -8,15 +9,23 @@ git clone https://github.com/VHSgunzo/superglue.git && cd superglue
 ```
 * **Usage**
 ```
-./superglue.py {runtime|script} {squashfs|archive}
+./superglue.py {small file} {large file}
 ```
-* Or for gluing fully in memory
+* Or for gluing fully in memory:
 ```
-./bufcat.py {runtime|script} {squashfs|archive}
+./bufcat.py {small file} {large file}
+```
+
+* Rust version:
+```
+./superglue {small file} {large file}
 ```
 
 * **You can also compile a binary Rust version**
 ```
-cargo build --release 
+rustup default nightly
+rustup target add x86_64-unknown-linux-musl
+rustup component add rust-src --toolchain nightly
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --release
 ```
 * Or take an already precompiled binary file from the [releases](https://github.com/VHSgunzo/superglue/releases)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::process::exit;
 use std::convert::TryInto;
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, SeekFrom, Write};
@@ -8,28 +8,23 @@ struct Cli {
     file_1_path: std::path::PathBuf,
 }
 
-static FILE_0_STR : &str = {
-    "ELF binary runtime or Shell script"
-};
-
-static FILE_1_STR : &str = {
-    "SquashFS image or Archive"
-};
+static FILE_0_STR : &str = "small file (ELF binary runtime or Shell script)";
+static FILE_1_STR : &str = "large file (SquashFS image or Archive)";
 
 fn main() {
 
     let file_0_path = std::env::args().nth(1)
         .unwrap_or_else(|| {
             eprintln!("Specify {}!", FILE_0_STR);
-            process::exit(1);
+            exit(1);
     });
 
     let file_1_path = std::env::args().nth(2)
         .unwrap_or_else(|| {
             eprintln!("Specify {}!", FILE_1_STR);
-            process::exit(1);
+            exit(1);
     });
-    
+
     let args = Cli {
         file_0_path: std::path::PathBuf::from(file_0_path),
         file_1_path: std::path::PathBuf::from(file_1_path),
@@ -42,13 +37,13 @@ fn main() {
         .open(args.file_0_path)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_0_STR, error);
-            process::exit(1);
+            exit(1);
     });
     let file_0_size = file_0.metadata().unwrap().len();
     let file_0_type = file_0.metadata().unwrap().file_type();
     if ! file_0_type.is_file() {
         eprintln!("{} is not a file!", FILE_0_STR);
-        process::exit(1);
+        exit(1);
     };
 
     let mut file_1 = OpenOptions::new()
@@ -58,26 +53,26 @@ fn main() {
         .open(args.file_1_path)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_1_STR, error);
-            process::exit(1);
+            exit(1);
     });
 
     let file_1_size = file_1.metadata().unwrap().len();
     let file_1_type = file_1.metadata().unwrap().file_type();
     if ! file_1_type.is_file() {
         eprintln!("{} is not a file!", FILE_1_STR);
-        process::exit(1);
+        exit(1);
     };
 
     if ! file_0_size <= file_1_size {
         eprintln!("It is necessary that {} is less than or equal to {}!", FILE_0_STR, FILE_1_STR);
-        process::exit(1);
+        exit(1);
     };
 
     let mut tmp_data_0 = vec![0u8; file_0_size.try_into().unwrap()];
     file_0.read_exact(&mut tmp_data_0)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_0_STR, error);
-            process::exit(1);
+            exit(1);
     });
 
     let parts_number = file_1_size / file_0_size;
@@ -90,13 +85,13 @@ fn main() {
     file_1.read_exact(&mut last_part_data)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_0_STR, error);
-            process::exit(1);
+            exit(1);
     });
-        
+
     file_1.set_len(parts_size)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_1_STR, error);
-            process::exit(1);
+            exit(1);
     });
 
     let mut seek_size = 0;
@@ -106,13 +101,13 @@ fn main() {
         file_1.read_exact(&mut tmp_data_1)
             .unwrap_or_else(|error| {
                 eprintln!("{}: {}", FILE_1_STR, error);
-                process::exit(1);
+                exit(1);
         });
         file_1.seek(SeekFrom::Start(seek_size)).unwrap();
         file_1.write_all(&tmp_data_0)
             .unwrap_or_else(|error| {
                 eprintln!("{}: {}", FILE_1_STR, error);
-                process::exit(1);
+                exit(1);
         });
         tmp_data_0 = tmp_data_1;
         seek_size += file_0_size;
@@ -122,6 +117,6 @@ fn main() {
     file_1.write_all(&last_part_data)
         .unwrap_or_else(|error| {
             eprintln!("{}: {}", FILE_1_STR, error);
-            process::exit(1);
+            exit(1);
     });
 }
